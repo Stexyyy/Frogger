7596bdeefec26636cb5a80bbbac902f67a9a6ea2
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2023-03-22 09:51:45 -0600

    Update frog.cpp

--- frog.cpp
+++ frog.cpp
@@ -11,3 +11,33 @@ frog::frog() {
 	xVel = 0;
 	yVel = 0;
 }
+
+//draw function draws small green square for frog
+void frog::draw(sf::RenderWindow& window) {
+	sf::RectangleShape FrogPic(sf::Vector2f(20, 20));
+	FrogPic.setFillColor(sf::Color(50, 200, 50));
+	FrogPic.setPosition(xpos, ypos);
+	window.draw(FrogPic);
+}
+
+//movement function for frog (unfinished)
+//this function takes a pointer to the array that holds the key presses
+void frog::jump(bool* keys) {
+	if (keys[UP] == true)
+		yVel = -50;
+	else if (keys[DOWN] == true)
+		yVel = 0;
+	else
+		yVel = 0;
+	ypos += yVel;
+}
+
+//this function kills the frog
+void frog::ded() {
+	//play explosion sound here
+	Beep(500, 500); // temporary until cool explosion
+	//hopefully animation here?
+	lives--;
+	xpos = 500;
+	ypos = 954;
+}
