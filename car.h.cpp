62312f6442f8fab65c247a05d5029ea6b3ff89a7
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2023-04-26 09:33:51 -0600

    Create car.h.cpp

---
+++ car.h.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+#include<iostream>
+#include<random>
+using namespace std;
+
+class car {
+	float xpos;
+	float ypos;
+	int green;
+	int blue;
+
+};
+//not finished
